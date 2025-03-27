168a51daf48b6686484d219022d46802089c23f6
woo yeong min <sjznsjx25792@gmail.com>
2025-03-27 20:10:08 +0900

    <Commit 3> sort 구현

---
+++ Algorithm/src/Sorting/Shell.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+public class Shell extends AbstractSort{
+    public static void sort(Comparable[] a){
+        int N = a.length;
+        int h = 1;
+        while(h < N/3) h = 3*h + 1;
+
+        while(h >= 1){
+            for(int i = h; i < N; i++)
+                for(int j = i; j >= h && less(a[j], a[j-1]); j-=h)
+                    exch(a, j, j-h);
+            h /= 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = {10, 4, 5, 2, 1, 8, 3, 6};
+        Shell.sort(a);
+        Shell.show(a);
+    }
+}
