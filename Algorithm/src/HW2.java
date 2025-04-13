3af67953a2c0b321ef56763bc48c2167e1033f87
woo yeong min <sjznsjx@naver.com>
2025-04-13 21:32:33 +0900

    <Commit 13> 알고리즘 HW2 최최최종본

--- Algorithm/src/HW2.java
+++ Algorithm/src/HW2.java
@@ -1,62 +1,74 @@
 import java.io.*;
 import java.util.*;
 
-public class HW2<K extends Comparable<K>, V extends Number & Comparable<V>> {
+public class HW2<K extends Comparable<K>, V> {
     static class Node<K, V> {
         K key;
         V value;
         Node<K, V> left, right;
-        int N;
 
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.N = 1;
         }
     }
 
     private Node<K, V> root;
 
-    private int size(Node<K, V> x) {
-        return (x == null) ? 0 : x.N;
-    }
-
-    public void put(K key, V value) {
-        root = put(root, key, value);
-    }
-
-    private Node<K, V> put(Node<K, V> x, K key, V value) {
-        if (x == null) return new Node<>(key, value);
+    public void put(K key, V val) {
+        if (root == null) {
+            root = new Node<>(key, val);
+            return;
+        }
+        Node<K, V> x = treeSearch(key);
         int cmp = key.compareTo(x.key);
-        if (cmp < 0) x.left = put(x.left, key, value);
-        else if (cmp > 0) x.right = put(x.right, key, value);
-        else x.value = value;
-        x.N = 1 + size(x.left) + size(x.right);
-        return x;
+        if (cmp == 0) {
+            x.value = val;
+        } else {
+            Node<K, V> newNode = new Node<>(key, val);
+            if (cmp < 0) {
+                x.left = newNode;
+            } else {
+                x.right = newNode;
+            }
+        }
     }
 
     public V get(K key) {
+        if (root == null) return null;
+        Node<K, V> x = treeSearch(key);
+        if (key.equals(x.key)) return x.value;
+        else return null;
+    }
+
+    protected Node<K, V> treeSearch(K key) {
         Node<K, V> x = root;
-        while (x != null) {
+        while (true) {
             int cmp = key.compareTo(x.key);
-            if (cmp == 0) return x.value;
-            else if (cmp < 0) x = x.left;
-            else x = x.right;
+            if (cmp == 0) return x;
+            else if (cmp < 0) {
+                if (x.left == null) return x;
+                else x = x.left;
+            } else {
+                if (x.right == null) return x;
+                else x = x.right;
+            }
         }
-        return null;
     }
 
     public Iterable<K> keys() {
+        if (root == null) return Collections.emptyList();
         ArrayList<K> keyList = new ArrayList<>();
         inorder(root, keyList);
         return keyList;
     }
 
     private void inorder(Node<K, V> x, ArrayList<K> keyList) {
-        if (x == null) return;
-        inorder(x.left, keyList);
-        keyList.add(x.key);
-        inorder(x.right, keyList);
+        if (x != null) {
+            inorder(x.left, keyList);
+            keyList.add(x.key);
+            inorder(x.right, keyList);
+        }
     }
 
     public int totalShingles() {
@@ -65,15 +77,13 @@ public class HW2<K extends Comparable<K>, V extends Number & Comparable<V>> {
 
     private int totalShingles(Node<K, V> x) {
         if (x == null) return 0;
-        return x.value.intValue() + totalShingles(x.left) + totalShingles(x.right);
+        return (Integer) x.value + totalShingles(x.left) + totalShingles(x.right);
     }
 
     public int commonShingles(HW2<K, V> other) {
         int count = 0;
         for (K key : this.keys()) {
-            if (other.get(key) != null) {
-                count++;
-            }
+            if (other.get(key) != null) count++;
         }
         return count;
     }
@@ -81,14 +91,11 @@ public class HW2<K extends Comparable<K>, V extends Number & Comparable<V>> {
     public int intersection(HW2<K, V> other) {
         int size = 0;
         for (K key : this.keys()) {
-            V valueThis = this.get(key);
-            V valueOther = other.get(key);
-            if (valueOther != null) {
-                if (valueThis.doubleValue() <= valueOther.doubleValue()) {
-                    size += valueThis.intValue();
-                } else {
-                    size += valueOther.intValue();
-                }
+            Integer v1 = (Integer) this.get(key);
+            Integer v2 = (Integer) other.get(key);
+            if (v2 != null) {
+                if (v1 < v2) size += v1;
+                else size += v2;
             }
         }
         return size;
@@ -97,65 +104,21 @@ public class HW2<K extends Comparable<K>, V extends Number & Comparable<V>> {
     public int union(HW2<K, V> other) {
         int size = 0;
         for (K key : this.keys()) {
-            V valueThis = this.get(key);
-            V valueOther = other.get(key);
-            if (valueOther != null) {
-                if (valueThis.doubleValue() >= valueOther.doubleValue()) {
-                    size += valueThis.intValue();
-                } else {
-                    size += valueOther.intValue();
-                }
+            Integer v1 = (Integer) this.get(key);
+            Integer v2 = (Integer) other.get(key);
+            if (v2 != null) {
+                if (v1 > v2) size += v1;
+                else size += v2;
             } else {
-                size += valueThis.intValue();
+                size += v1;
             }
         }
         for (K key : other.keys()) {
             if (this.get(key) == null) {
-                size += other.get(key).intValue();
+                size += (Integer) other.get(key);
             }
         }
         return size;
-    }
-
-    private static HW2<String, Integer> createBSTFromFile(String fileName) {
-        HW2<String, Integer> bst = new HW2<>();
-
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String line;
-            ArrayList<String> tokens = new ArrayList<>();
-            while ((line = reader.readLine()) != null) {
-                StringTokenizer st = new StringTokenizer(line, " \t\n=;,<>()");
-                while (st.hasMoreTokens()) {
-                    tokens.add(st.nextToken());
-                }
-            }
-            if (tokens.size() >= 5) {
-                for (int i = 0; i <= tokens.size() - 5; i++) {
-                    String shingle = tokens.get(i) + " " + tokens.get(i+1) + " " +
-                            tokens.get(i+2) + " " + tokens.get(i+3) + " " + tokens.get(i+4);
-                    Integer count = bst.get(shingle);
-                    if (count == null) {
-                        bst.put(shingle, 1);
-                    } else {
-                        bst.put(shingle, count + 1);
-                    }
-                }
-            }
-            reader.close();
-        } catch (IOException e) {
-            System.out.println("파일 읽기 오류: " + e.getMessage());
-        }
-        return bst;
-    }
-
-    private static double Similarity(HW2<String, Integer> bstA, HW2<String, Integer> bstB) {
-        int intersectionSize = bstA.intersection(bstB);
-        int unionSize = bstA.union(bstB);
-        if (unionSize == 0) {
-            return 0.0;
-        }
-        return (double) intersectionSize / unionSize;
     }
 
     public static void main(String[] args) {
@@ -167,15 +130,65 @@ public class HW2<K extends Comparable<K>, V extends Number & Comparable<V>> {
         System.out.print("두번째 파일 이름? ");
         String fileB = scanner.nextLine();
 
-        HW2<String, Integer> bstA = createBSTFromFile(fileA);
-        HW2<String, Integer> bstB = createBSTFromFile(fileB);
+        HW2<String, Integer> bstA = new HW2<>();
+        HW2<String, Integer> bstB = new HW2<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileA));
+            ArrayList<String> tokens = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(line, " \t\n=;,<>()");
+                while (st.hasMoreTokens()) tokens.add(st.nextToken());
+            }
+            for (int i = 0; i <= tokens.size() - 5; i++) {
+                String shingle = String.join(" ", tokens.subList(i, i + 5));
+                Integer count = bstA.get(shingle);
+                if (count == null) {
+                    bstA.put(shingle, 1);
+                } else {
+                    bstA.put(shingle, count + 1);
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("파일 " + fileA + " 읽기 오류: " + e.getMessage());
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileB));
+            ArrayList<String> tokens = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(line, " \t\n=;,<>()");
+                while (st.hasMoreTokens()) tokens.add(st.nextToken());
+            }
+            for (int i = 0; i <= tokens.size() - 5; i++) {
+                String shingle = String.join(" ", tokens.subList(i, i + 5));
+                Integer count = bstB.get(shingle);
+                if (count == null) {
+                    bstB.put(shingle, 1);
+                } else {
+                    bstB.put(shingle, count + 1);
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("파일 " + fileB + " 읽기 오류: " + e.getMessage());
+        }
 
         int shinglesA = bstA.totalShingles();
         int shinglesB = bstB.totalShingles();
-
         int commonShingles = bstA.commonShingles(bstB);
+        int union = bstA.union(bstB);
+        int intersection = bstA.intersection(bstB);
+        double similarity;
 
-        double similarity = Similarity(bstA, bstB);
+        if (union == 0) {
+            similarity = 0.0;
+        } else {
+            similarity = (double) intersection / union;
+        }
 
         System.out.println("파일 " + fileA + "의 Shingle의 수 = " + shinglesA);
         System.out.println("파일 " + fileB + "의 Shingle의 수 = " + shinglesB);
@@ -184,6 +197,4 @@ public class HW2<K extends Comparable<K>, V extends Number & Comparable<V>> {
 
         scanner.close();
     }
-
-
 }
